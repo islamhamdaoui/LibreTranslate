a5c0d55dd78caf0262c44f62822fa9c7e3d4e79f
islamhamdaoui <islamhamdaoui2000@gmail.com>
2025-04-08 17:19:30 +0100

    added languages i need

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ rm -rf /tmp/prometheus_data/*
 
 # Set environment variables to limit model loading
 export LT_UPDATE_MODELS=false
-export LT_LOAD_ONLY=en,es,fr,de,it,pt,ru,zh,ja,ko
+export LT_LOAD_ONLY=en,es,fr,de,it,pt,ru,ja,ko,zh,ar,tr,hi,id
 
 # Start the application using gunicorn with the wsgi entry point
 exec gunicorn --bind 0.0.0.0:$PORT --timeout 300 --workers 1 'wsgi:app' 
